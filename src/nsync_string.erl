d381d9e12e982446f33dd25bccd8ce3700fc9591
José Manuel Rodríguez Blanco <josemanuel.rodriguezblanco@gmail.com>
2011-05-03 21:36:15 +0200

    FUNCT Added setex and incr functionalities to nsync_string (no real functionality)

--- src/nsync_string.erl
+++ src/nsync_string.erl
@@ -24,10 +24,17 @@
 -export([command_hooks/0, handle/3]).
 
 command_hooks() ->
-    ["set", "del"].
+    ["set", "del", "setex", "incr"].
 
 handle("set", [Key, Val], Tid) ->
     ets:insert(Tid, {Key, Val});
 
 handle("del", [Key], Tid) ->
     ets:delete(Tid, Key).
+
+handle("setex", [_Key, _TTL, _Val], Tid) ->
+    ok;
+
+handle("incr", [_Key], Tid) ->
+    ok.
+
